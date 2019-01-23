2daca0684e6f66e5d820c089a287e773e8e8da9e
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-01-23 06:27:34 +0100

    Add Forms

--- src/App.js
+++ src/App.js
@@ -11,6 +11,78 @@ import Projects from "./components/projects";
 import Confetti from "./components/common/confetti";
 import "./App.css";
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { formName: "Please enter Your name", textArea: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputClick = this.handleInputClick.bind(this);
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ formName: event.target.value });
+  }
+  handleTextAreaChange(event) {
+    this.setState({ textArea: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(
+      "A name was submitted: " +
+        this.state.formName +
+        "with an Essay: " +
+        this.state.textArea
+    );
+    event.preventDefault();
+  }
+
+  handleInputClick(event) {
+    this.setState({ formName: "" });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <h2>What's Your Name?</h2>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.formName}
+              onChange={this.handleChange}
+              onClick={this.handleInputClick}
+            />
+          </label>
+          <label>
+            Essay:
+            <textarea
+              value={this.state.textArea}
+              onChange={this.handleTextAreaChange}
+            />
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+        <div className="cointainer">
+          {" "}
+          Name: {this.state.formName} <br />
+          Essay: {this.state.textArea}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+const forms = () => {
+  return (
+    <React.Fragment>
+      <NameForm />
+    </React.Fragment>
+  );
+};
+
 const home = () => {
   return (
     <React.Fragment>
@@ -145,6 +217,7 @@ class App extends Component {
             <Route path="/skills" component={skills} />
             <Route path="/games" component={games} />
             <Route path="/resources" component={Resources} />
+            <Route path="/forms" component={forms} />
             <Route path="/contact" component={contact} />
             <Route path="/" exact component={home} />
             <Route path="/" component={NotFound} />
