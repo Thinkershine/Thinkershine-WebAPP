7c893f717d3d3651c1933b2f67cddf7c6033a72a
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-01-25 20:34:29 +0100

    Create GuestBook from NameForm

--- src/components/forms/nameForm.jsx
+++ src/components/forms/nameForm.jsx
@@ -3,63 +3,90 @@ import React, { Component } from "react";
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { formName: "Please enter Your name", textArea: "" };
+    this.state = {
+      name: "Please enter Your name",
+      nameClicked: false,
+      messageArea: "Here You Can Write an message.",
+      messageClicked: false,
+      submitted: { name: "", message: "", submitted: false }
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputClick = this.handleInputClick.bind(this);
-    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    this.handleNameInputClick = this.handleNameInputClick.bind(this);
+    this.handlemessageAreaChange = this.handlemessageAreaChange.bind(this);
+    this.handlemessageAreaClick = this.handlemessageAreaClick.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ formName: event.target.value });
+    this.setState({ name: event.target.value });
   }
-  handleTextAreaChange(event) {
-    this.setState({ textArea: event.target.value });
+
+  handlemessageAreaChange(event) {
+    this.setState({ messageArea: event.target.value });
+  }
+
+  handlemessageAreaClick(event) {
+    this.setState({ messageArea: "" });
   }
 
   handleSubmit(event) {
-    alert(
-      "A name was submitted: " +
-        this.state.formName +
-        "with an Essay: " +
-        this.state.textArea
-    );
+    console.log("SUBMIT");
+    this.setState({
+      submitted: {
+        name: this.state.name,
+        message: this.state.messageArea,
+        submitted: true
+      }
+    });
     event.preventDefault();
   }
 
-  handleInputClick(event) {
-    this.setState({ formName: "" });
+  handleNameInputClick(event) {
+    this.setState({ name: "" });
   }
 
   render() {
     return (
       <React.Fragment>
         <form onSubmit={this.handleSubmit}>
+          <h1>GuestBook</h1>
           <h2>What's Your Name?</h2>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={this.state.formName}
-              onChange={this.handleChange}
-              onClick={this.handleInputClick}
-            />
-          </label>
-          <label>
-            Essay:
-            <textarea
-              value={this.state.textArea}
-              onChange={this.handleTextAreaChange}
-            />
-          </label>
-          <input type="submit" value="submit" />
+          <div className="form-group">
+            <label>
+              Name:
+              <input
+                className="form-control"
+                type="text"
+                value={this.state.name}
+                placeholder={this.state.name}
+                onChange={this.handleChange}
+                onClick={this.handleNameInputClick}
+              />
+              <small className="form-text text-muted">Say Hi!</small>
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Message:
+              <textarea
+                className="form-control"
+                value={this.state.messageArea}
+                placeholder={this.state.messageArea}
+                onChange={this.handlemessageAreaChange}
+                onClick={this.handlemessageAreaClick}
+              />
+            </label>
+          </div>
+          <input type="submit" value="submit" className="btn btn-success" />
         </form>
-        <div className="cointainer">
-          {" "}
-          Name: {this.state.formName} <br />
-          Essay: {this.state.textArea}
-        </div>
+        {this.state.submitted.submitted && (
+          <div className="cointainer">
+            {" "}
+            Name: {this.state.submitted.name} <br />
+            message: {this.state.submitted.message}
+          </div>
+        )}
       </React.Fragment>
     );
   }
