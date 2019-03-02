1280af93e26b03044608ae476baa1b86ab287cec
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-03-02 21:16:51 +0100

    Add Redux for PlayerData State Handling

---
+++ src/components/widgets/todo.jsx
@@ -0,0 +1,11 @@
+import { createStore } from "react";
+import * as types from "./constants/ActionTypes";
+
+class ToDoWidget extends Component {
+  state = {};
+  render() {
+    return <div id="todo" />;
+  }
+}
+
+export default ToDoWidget;
